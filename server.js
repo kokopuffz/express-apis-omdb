b5fb51ebcc99267e1d80f86da47f901951a8d689
kokopuffz <chanheeya@gmail.com>
2022-02-19 21:11:53 -0600

    controller for movie results

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios')
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const PORT = 8000
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -16,12 +18,45 @@ app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+// app.get('/', function(req, res) {
+//   res.send('Hello, backend!');
+// });
+
+
+//main start page
+app.get('/', (req,res)=>{
+  res.render('index.ejs')
+})
+
+app.get("/results", (req, res) => {
+  console.log(req.query.search);
+  const url = `http://www.omdbapi.com/?t=${req.query.search}&plot=full&apikey=${process.env.OMDB_API_KEY}`;
+  console.log(url);
+  axios.get(url).then((response) => {
+    const searchResults = response.data;
+    console.log(searchResults);
+    res.render("results.ejs", { results: searchResults });
+    if (!searchResults.response){
+      res.send('movie does not exist, try again')
+    }
+  });
 });
 
+
+
+// app.get('/detail', (req,res) => {
+
+// }) 
+
+// app.use('/results', require('./controllers/moviesController.js'))
+
+app.use('/movies', require('./controllers/moviesController.js'))
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+// var server = app.listen(process32.env.PORT || 8000);
+app.listen(PORT, err =>{
+  if (err) console.log(err)
+  console.log(`listening to port ${PORT}. r we there yet?`)
+})
 
 // We can export this server to other servers like this
-module.exports = server;
+// module.exports = server;
